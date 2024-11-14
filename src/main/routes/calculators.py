0aa8dc58b5be8f02fef9633a7884a0337ab08135
Matheus Ferreira <matheusfdantas03@gmail.com>
2024-11-13 21:42:01 -0300

    feat: implement firt calculator

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, jsonify, request
+from src.calculators.calculator_1 import Calculator1
 
 calc_route_bp = Blueprint("calc_route", __name__)
 
 @calc_route_bp.route("/calculator/1", methods=["POST"])
 def calculator_1():
-    print(request)
-    return jsonify({"success": True}), 200
+    calc = Calculator1()
+    response = calc.calculate(request)
+    return jsonify(response), 200
